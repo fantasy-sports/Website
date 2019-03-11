c47f2d4ddbdd07b6d54494a9ba3cbed05e96b894
jessclifford8 <47761511+jessclifford8@users.noreply.github.com>
2019-03-11 17:33:11 -0400

    Add files via upload
    This is the updated fancyoptimizer.py for video 4.
    To run this, do a pip install flask-sqlalchemy in the terminal at the very beginning.
    After writing all the code, do "python," then "from fancyoptimizer import db" in the terminal followed by "db.create_all()" to set up the database. (17:55 in video 4)

---
+++ fancyoptimizer.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'db9439c2b827d2085562a1e0b4bee8bc' # secret key to protect forms from attacks - https://youtu.be/UIJKdCIEXUQ (10:20)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fancysite.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+posts = [
+    {
+        'author': 'Michael Muchane',
+        'title': 'Hot Picks: March 3rd',
+        'content': 'First post content',
+        'date_posted': '9:46am, 03/03/19'
+    },
+    {
+        'author': 'Sam Lubell',
+        'title': 'Hot Picks: March 4th',
+        'content': 'Second post content',
+        'date_posted': '10:01am, 03/04/19'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@optimizer.com' and form.password.data == 'password': # temporary login credentials - https://youtu.be/UIJKdCIEXUQ (42:15)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
